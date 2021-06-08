7b9abf1160255428f4f95aa4b4f195651f7434d4
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2021-06-07 18:25:02 -0700

    bhbhhbhbhbbhb

--- src/_01_static_smiley_blues/SmileyBlues.java
+++ src/_01_static_smiley_blues/SmileyBlues.java
@@ -1,15 +1,20 @@
 package _01_static_smiley_blues;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class SmileyBlues {
 	void start() {
 		// 1. Make a new JFrame and set it to be visible
-
+JFrame jf = new JFrame();
+jf.setVisible(true);
 		// 2. Set your frame's default close operation to JFrame.EXIT_ON_CLOSE
-
+jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
 		// 3. Make a new JPanel
-
+JPanel jp = new JPanel();
 		// 4. Add your panel to your frame
-
+jf.add(jp);
+jf.pack();
 		// 8. DO THIS LAST: 
 		// Set the Smiley class's color variable to Smiley.YELLOW (NOT a specific smiley object's!)
 		// Using a specific object for a static variable works, but is not usually recommended.
